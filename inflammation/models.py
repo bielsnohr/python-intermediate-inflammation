de17c1e64d275c5d2ce36a2e0e73fe354a250ecd
bielsnohr <matthew.bluteau@gmail.com>
2021-06-28 14:29:00 +0100

    Capitalise 'D' in 1 and 2-dimensional acronym

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,32 +15,32 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array.
+    """Calculate the daily mean of a 2D inflammation data array.
 
-    :param data: 2d inflammation data array. 0th axis (rows) are patients
+    :param data: 2D inflammation data array. 0th axis (rows) are patients
     indexes, and 1st axis (columns) are the day index.
-    :returns: 1d array with the daily mean for all patients' inflammation values
+    :returns: 1D array with the daily mean for all patients' inflammation values
     """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array.
+    """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: 2d inflammation data array. 0th axis (rows) are patients
+    :param data: 2D inflammation data array. 0th axis (rows) are patients
     indexes, and 1st axis (columns) are the day index.
-    :returns: 1d array with the daily maximum for all patients' inflammation
+    :returns: 1D array with the daily maximum for all patients' inflammation
     values
     """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array.
+    """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: 2d inflammation data array. 0th axis (rows) are patients
+    :param data: 2D inflammation data array. 0th axis (rows) are patients
     indexes, and 1st axis (columns) are the day index.
-    :returns: 1d array with the daily minimum for all patients' inflammation
+    :returns: 1D array with the daily minimum for all patients' inflammation
     values
     """
     return np.min(data, axis=0)

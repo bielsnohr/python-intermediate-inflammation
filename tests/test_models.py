1112aad18e5aa9b105edfc822c5ebd2f759f709c
bielsnohr <matthew.bluteau@gmail.com>
2021-06-29 15:50:59 +0100

    Add test of invalid data for daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -54,4 +55,9 @@ def test_daily_min_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
 
-# TODO(lesson-robust) Implement tests for the other statistical functions
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
